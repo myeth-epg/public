5c1e218e9a114ce16eacf66c1c281a910cb5a818
myeth-epg <panda.goes.to.the.moon@gmail.com>
2026-08-13 22:18:46 +0800

    Add files via upload

--- xml-epg.js
+++ xml-epg.js
@@ -127,7 +127,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const regions = [
     { name: 'Main a', url: 'index.html' },
     { name: 'Main b', url: 'index-2.html' },
-    { name: 'Main 2t', url: 'index-2t.html' },
+    { name: 'Main 2', url: 'index-2t.html' },
     { name: 'Hong Kong', url: 'index-hk.html' },
     { name: 'Taiwan', url: 'index-tw.html' },
     { name: 'Malaysia', url: 'index-my.html' },
